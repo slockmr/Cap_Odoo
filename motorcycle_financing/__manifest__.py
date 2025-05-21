f0aa87ce6fc9af67e3ab14a644e1ffad1d101bbe
slockmr <slock.mr@gmail.com>
2025-05-21 19:01:29 +0000

    Se guardo manifest

---
+++ motorcycle_financing/__manifest__.py
@@ -0,0 +1,14 @@
+{
+	​'name': 'Motorcycle Financing',
+	​'summary': 'Streamlines the loan application process for dealerships.',
+	​'description': '',
+	​'license': 'OPL-1',
+	​'category': 'Kawiil/Custom Modules',
+	​'author': 'misael',
+	​'website': 'https://github.com/slockmr/Cap_Odoo.git',
+	​'version': '18.0.0.0.1',
+	​'depends': [],
+	​'data': [],
+	​'demo': [],
+	​'application': 'true',
+}
