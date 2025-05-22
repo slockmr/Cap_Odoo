a2e1a40d752e119fffc838aa4fe542ef48b674d2
slockmr <slock.mr@gmail.com>
2025-05-22 17:02:18 +0000

    tarea 2

--- motorcycle_financing/__manifest__.py
+++ motorcycle_financing/__manifest__.py
@@ -9,11 +9,6 @@
 	'version': '18.0.0.0.1',
 	'depends': [],
 	'data': [
-        "security/res_groups.xml",
-        "security/ir.model.access.csv",
-        "security/rules.xml",
-        "views/loan_application_views.xml",
-        "views/motorcycle_financing_menu.xml",
 
     ],
 	'demo': ['data/loan_demo.xml'],
