81a9940421a1fce3baeb8866ff5a3d095d2e1d7f
slockmr <slock.mr@gmail.com>
2025-05-22 16:51:27 +0000

    tarea 2.03

--- motorcycle_financing/__manifest__.py
+++ motorcycle_financing/__manifest__.py
@@ -8,7 +8,14 @@
 	'website': 'https://github.com/slockmr/Cap_Odoo.git',
 	'version': '18.0.0.0.1',
 	'depends': [],
-	'data': [],
-	'demo': [],
-	'application': 'true',
+	'data': [
+        "security/res_groups.xml",
+        "security/ir.model.access.csv",
+        "security/rules.xml",
+        "views/loan_application_views.xml",
+        "views/motorcycle_financing_menu.xml",
+
+    ],
+	'demo': ['data/loan_demo.xml'],
+	'application': True,
 }
